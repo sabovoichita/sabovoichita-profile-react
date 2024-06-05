e610cde317085e91db38f33fdf15a0650c6083f3
Voichita Maria <sabo_voichita@yahoo.com>
2024-06-05 11:02:59 +0200

    add LanguagesTable

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,37 @@
 // import React from "react";
 // import logo from "./logo.svg";
 import "./App.css";
+import { LanguagesTable } from "./LanguagesTable/LanguagesTable";
 
+let languages = [
+  {
+    name: "Romanian",
+    level: "Native",
+  },
+  {
+    name: "English",
+    level: "Profesional",
+  },
+  {
+    name: "Spanish",
+    level: "Beginner",
+  },
+  {
+    name: "French",
+    level: "Beginner",
+  },
+];
 function App() {
   return (
-    <div id="header-info">
-      <h1>Voichita Maria</h1>
-      <p id="job-title">Transport Manager</p>
-    </div>
+    <>
+      <div id="header-info">
+        <h1>Voichita Maria</h1>
+        <p id="job-title">Transport Manager</p>
+        <div>
+          <LanguagesTable languages={languages} />
+        </div>
+      </div>
+    </>
   );
 }
 
---
+++ src/LanguagesTable/LanguagesTable.js
@@ -0,0 +1,40 @@
+export const LanguagesTable = (props) => (
+  <table border={1}>
+    <thead>
+      <tr>
+        <th>Language</th>
+        <th>Level</th>
+        <th></th>
+      </tr>
+    </thead>
+    <tbody>
+      {props.languages.map((language, index) => (
+        <tr key={index}>
+          <td className="favorite">{language.name}</td>
+          <td>{language.level}</td>
+          <td>
+            <a href="#" className="delete-row">
+              &#10006;
+            </a>
+            <a href="#" className="edit-row">
+              &#9998;
+            </a>
+          </td>
+        </tr>
+      ))}
+    </tbody>
+    <tfoot>
+      <tr>
+        <td>
+          <input type="text" name="language" placeholder="Enter Language" />
+        </td>
+        <td>
+          <input type="text" name="level" placeholder="Enter Level" />
+        </td>
+        <td>
+          <button>Save</button>
+        </td>
+      </tr>
+    </tfoot>
+  </table>
+);
