0088b65868246bea43635452cc62b79a87bd0118
Voichita Maria <sabo_voichita@yahoo.com>
2024-06-10 12:18:29 +0200

    add footer

--- src/App.js
+++ src/App.js
@@ -5,6 +5,10 @@ import { connect } from "react-redux";
 import { FilterContainer } from "./filter";
 import selfie from "./images/selfie.jpg";
 import light from "./images/pic_bulbon.gif";
+import linkedin from "./images/linkedin.svg";
+import github from "./images/github.svg";
+import youtube from "./images/youtube.svg";
+import email from "./images/email-round.svg";
 
 class App extends Component {
   constructor(props) {
@@ -77,6 +81,33 @@ class App extends Component {
             </li>
           </ul>
         </header>
+
+        <footer>
+          <a
+            href="https://www.linkedin.com/in/voichita-maria-truscai/"
+            title="LinkedIn"
+            target="_blank"
+          >
+            <img src={linkedin} width="40px" alt="Linkedin Logo" />
+          </a>
+          <a
+            href="https://github.com/sabovoichita"
+            title="GitHub"
+            target="_blank"
+          >
+            <img src={github} width="40px" alt="Github logo" />
+          </a>
+          <a
+            href="https://www.youtube.com/channel/UCUFk52XTij2scqh_E9jlPiw"
+            title="Youtube"
+            target="_blank"
+          >
+            <img src={youtube} width="40px" alt="Youtube logo" />
+          </a>
+          <a href="mailto:sabo_voichita4sacrifice@yahoo.com">
+            <img src={email} width="40px" alt="email" />
+          </a>
+        </footer>
         <div>
           <FilterContainer />
         </div>
